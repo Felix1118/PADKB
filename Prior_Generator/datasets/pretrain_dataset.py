c23c2fee86b83d7a389bb513359287e1209a8b3f
Felix1118 <shiyigao27@gmail.com>
2025-12-03 13:38:46 +0800

    Add files via upload

---
+++ Prior_Generator/datasets/pretrain_dataset.py
@@ -0,0 +1,213 @@
+import os
+import pickle
+import re
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data as data
+from nltk.tokenize import RegexpTokenizer
+from constants import *
+from datasets.utils import get_imgs
+from tqdm import tqdm
+from transformers import BertTokenizer
+from collections import defaultdict
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class MultimodalPretrainingDataset(data.Dataset):
+    def __init__(self, split="train", transform=None, data_pct=1.0,
+                 imsize=256, max_words=112, sent_num=3):
+        super().__init__()
+
+        self.transform = transform
+        self.imsize = imsize
+        self.df = pd.read_csv(MIMIC_CXR_MASTER_CSV)   
+        self.tokenizer = BertTokenizer.from_pretrained(
+            "./Bert_pretrained")
+        self.max_words = max_words
+
+    def load_text_data(self, split):
+        # get study to captions mapping
+        # TODO: check this
+        filepath = os.path.join(
+            BASE_DIR, "../../data/captions.pickle")
+        if not os.path.isfile(filepath):
+            print(
+                f"Caption file {filepath} does not exit. Creating captions...")
+            path2sent = self.create_path_2_sent_mapping()
+            with open(filepath, "wb") as f:
+                pickle.dump(path2sent, f, protocol=2)
+                print("Save to: ", filepath)
+        else:
+            with open(filepath, "rb") as f:
+                path2sent = pickle.load(f)
+
+        # filter studies to use for current split
+        filenames = []
+        for row in self.df.itertuples():
+            cur_split = getattr(row, MIMIC_CXR_SPLIT_COL)
+            path = getattr(row, MIMIC_CXR_PATH_COL)
+            if cur_split == split and path in path2sent:
+                filenames.append(path)
+
+        return filenames, path2sent
+
+    def create_path_2_sent_mapping(self):
+        sent_lens, num_sents = [], []
+        path2sent = {}
+        # iterrows is not faster than itertuples ...  but it is ok
+        for _, row in tqdm(self.df.iterrows(), total=self.df.shape[0]):
+            # pick impression, findings, last_paragraph
+            captions = ""
+            captions += row["impression"]
+            captions += " "
+            captions += row["findings"]
+
+            # use space instead of newline
+            captions = captions.replace("\n", " ")
+
+            # split sentences
+            splitter = re.compile("[0-9]+\.")
+            captions = splitter.split(captions)
+            captions = [point.split(".") for point in captions]
+            captions = [sent for point in captions for sent in point]
+
+            cnt = 0
+            study_sent = []
+            # create tokens from captions
+            for cap in captions:
+                if len(cap) == 0:
+                    continue
+
+                cap = cap.replace("\ufffd\ufffd", " ")
+                # picks out sequences of alphanumeric characters as tokens
+                # and drops everything else
+                tokenizer = RegexpTokenizer(r"\w+")
+                tokens = tokenizer.tokenize(cap.lower())
+                # TODO: < 3 has instances of ['no', 'pneumothorax'], ['clear', 'lung']
+                if len(tokens) <= 1:
+                    continue
+
+                # filter tokens for current sentence
+                included_tokens = []
+                for t in tokens:
+                    t = t.encode("ascii", "ignore").decode("ascii")
+                    if len(t) > 0:
+                        included_tokens.append(t)
+
+                if len(included_tokens) > 0:
+                    study_sent.append(" ".join(included_tokens))
+
+                cnt += len(included_tokens)
+
+            if cnt >= 3:
+                sent_lens.append(cnt)
+                num_sents.append(len(study_sent))
+                path2sent[row[MIMIC_CXR_PATH_COL]] = study_sent
+
+        # get report word/setence statistics
+        sent_lens = np.array(sent_lens)
+        num_sents = np.array(num_sents)
+
+        print(
+            f"sent lens: {sent_lens.min()},{sent_lens.mean()},{sent_lens.max()} [{np.percentile(sent_lens, 5)}, {np.percentile(sent_lens, 95)}]"
+        )
+        print(
+            f"num sents: {num_sents.min()},{num_sents.mean()},{num_sents.max()} [{np.percentile(num_sents, 5)}, {np.percentile(num_sents, 95)}]"
+        )
+
+        return path2sent
+
+    def __len__(self):
+        return len(self.df)
+
+    def get_caption(self, captions,entities):
+        series_sents=re.split(r'\.+', captions) 
+        series_sents = list(filter(lambda x: x != "", series_sents))
+        sent = " ".join(series_sents)
+        tokens = self.tokenizer(
+            sent,
+            return_tensors="pt",
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_words,
+        )
+        input_ids=tokens['input_ids'].squeeze().tolist()
+        entity_tokens=self.tokenizer.tokenize(entities)
+        entity_token_ids = self.tokenizer.convert_tokens_to_ids(entity_tokens)
+        token_to_indices = defaultdict(list)
+        for idx, token in enumerate(input_ids):
+             if token in entity_token_ids:  
+                token_to_indices[token].append(idx)
+        all_indices = sorted({idx for indices in token_to_indices.values() for idx in indices})
+        x_len = len([t for t in tokens["input_ids"][0] if t != 0])
+        if(len(all_indices)<=1):
+            all_indices=list(range(x_len))
+        
+        entity_index=torch.tensor(all_indices).unsqueeze(0)
+        tokens['entity_index']=entity_index
+
+        
+
+        return tokens, x_len
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+        key=row['Path']
+        captions=row['Report Impression']
+        entities=row['Entities']
+        caps, cap_len = self.get_caption(captions,entities)
+        imgs = get_imgs(key, self.imsize, self.transform, multiscale=False)
+        return imgs, caps, cap_len, key
+
+
+def multimodal_collate_fn(batch):
+    """sort sequence"""
+    imgs, cap_len, ids, tokens, attention,entity_index = [], [], [], [], [], []
+    path = []
+    for b in batch:
+        img, cap, cap_l, p = b
+        imgs.append(img)
+        cap_len.append(cap_l)
+        ids.append(cap["input_ids"])
+        tokens.append(cap["token_type_ids"])
+        attention.append(cap["attention_mask"])
+        entity_index.append(cap['entity_index'])
+        path.append(p)
+
+    # stack
+    imgs = torch.stack(imgs)
+    ids = torch.stack(ids).squeeze()
+    tokens = torch.stack(tokens).squeeze()
+    attention = torch.stack(attention).squeeze()
+    entity_index=torch.stack(entity_index).squeeze()
+
+    # sort and add to dictionary
+    sorted_cap_lens, sorted_cap_indices = torch.sort(
+        torch.tensor(cap_len), 0, True)
+
+    path = np.array(path)
+    
+   
+    
+    return_dict = {
+        "caption_ids": ids,
+        "token_type_ids": tokens,
+        "attention_mask": attention,
+        "entity_position_index": entity_index,
+        "imgs": imgs,
+        "cap_lens": sorted_cap_lens,
+        'shape':entity_index.shape,
+        "path": path
+    }
+    return return_dict
+
+
+if __name__ == "__main__":
+    from mgca.datasets.transforms import DataTransforms
+    transform = DataTransforms(is_train=True)
+    dataset = MultimodalPretrainingDataset(split="train", transform=transform)
+    data = dataset[0]
+    print(data)
